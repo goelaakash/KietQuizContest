afd4fb8d2af9c66908d5c7ae711e6aaa86afda37
Devesh Goel <devesh97goel@gmail.com>
2021-06-15 11:32:22 +0530

    Logout

--- app/src/main/java/com/aakash/quizapp/HomeActivity.java
+++ app/src/main/java/com/aakash/quizapp/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.aakash.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,16 +22,34 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        b1=findViewById(R.id.btn_logout);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                auth = FirebaseAuth.getInstance();
-                auth.signOut();
-                finish();
-                startActivity(new Intent(HomeActivity.this, MainActivity.class));
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.feed_toolbar);
+        setSupportActionBar(toolbar);
+
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater()
+                .inflate(R.menu.navigation, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected( final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.findfriend:
+                startActivity(new Intent(HomeActivity.this,EditProfileActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                break;
+            case R.id.log_out:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+    }
+
 }
